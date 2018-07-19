15a7adc47af5dfc7c2e41d0b0f9c1edee61090f7
14260 <1426079590@qq.com>
2018-07-19 18:44:30 +0800

    20170709

---
+++ pages/express/express.js
@@ -0,0 +1,102 @@
+// pages/exprss/exprss.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    wuliu: {
+      "LogisticCode": "634067340947",
+      "ShipperCode": "ZTO",
+      "Traces": [{
+        "AcceptStation": "【南昌市】  【南昌二部】（0791-88187848） 的 辛斌13677098502 （13677098502） 已揽收",
+        "AcceptTime": "2018-06-27 19:43:46"
+      }, {
+        "AcceptStation": "【南昌市】  快件离开 【南昌二部】 发往 【南昌】",
+        "AcceptTime": "2018-06-27 19:44:18"
+      }, {
+        "AcceptStation": "【南昌市】  快件到达 【南昌中转部】",
+        "AcceptTime": "2018-06-27 20:59:41"
+      }, {
+        "AcceptStation": "【南昌市】  快件离开 【南昌中转部】 发往 【盘锦中转】",
+        "AcceptTime": "2018-06-27 21:06:14"
+      }, {
+        "AcceptStation": "【盘锦市】  快件到达 【盘锦中转】",
+        "AcceptTime": "2018-06-29 06:52:33"
+      }, {
+        "AcceptStation": "【盘锦市】  快件离开 【盘锦中转】 发往 【阜新】",
+        "AcceptTime": "2018-06-29 06:52:51"
+      }, {
+        "AcceptStation": "【阜新市】  快件到达 【阜新】",
+        "AcceptTime": "2018-06-29 12:52:03"
+      }, {
+        "AcceptStation": "【阜新市】  【阜新】 的八二18841820281（18841820281） 正在第1次派件, 请保持电话畅通,并耐心等待",
+        "AcceptTime": "2018-06-29 14:34:14"
+      }, {
+        "AcceptStation": "【阜新市】  快件已在 【阜新】 签收,签收人: 本人, 感谢使用中通快递,期待再次为您服务!",
+        "AcceptTime": "2018-06-29 17:23:58"
+      }],
+      "State": "3",
+      "EBusinessID": "1363575",
+      "Success": true
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
